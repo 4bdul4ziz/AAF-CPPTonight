53a4068740d58c2540399b00fa204efb6601c3c9
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-09-07 18:35:09 +0530

    reverting changes

--- src/django_project/settings.py
+++ src/django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "1c5abbe7756b47c3c02e9104696beec38f605f27"
 
 if SECRET_KEY is None:
   print("Please setup a SECRET_KEY in the Secrets (Environment variables) tab. See README.md for more.")
